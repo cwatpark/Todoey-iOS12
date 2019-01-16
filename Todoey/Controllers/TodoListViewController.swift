4b891b316bc0df36767b73697ace1115dc501d55
Ice <ice@macbook-pro-4.local>
2019-01-16 17:03:01 +0700

    add searchBar

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -13,6 +13,8 @@ class TodoListViewController: UITableViewController {
   
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 
+        //let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
         self.loadItems()
         
     }
@@ -45,6 +47,11 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Medthods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //itemArray[indexPath.row].setValue("Complete", forKey: "title")  <-- UPDATE DATA IN CORE DATA
+        
+        //context.delete(itemArray[indexPath.row]) <-- Delete Data From CoreData
+        //itemArray.remove(at: indexPath.row)
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
@@ -97,15 +104,41 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(){
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest() ){
+        //Item.fetchRequest() is a ddefault value if didnt havre parameter
         do{
             itemArray =  try context.fetch(request)
         }catch{
             print("Error cant fetch context \(error)")
         }
+        self.tableView.reloadData()
     }
     
 }
 
+//MARK: - Search Bar Methods
 
+extension TodoListViewController: UISearchBarDelegate{
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+            request.predicate = NSPredicate(format: "title CONTAINS %@", searchBar.text!)
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+
+            loadItems(with: request)
+        
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            self.loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            } //Hidekeyboard When click cancle search
+            
+        }
+    }
+    
+}
